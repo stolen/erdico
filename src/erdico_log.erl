d23c837f3e701daea47444a51ed92d17a0433d95
Danil Zagoskin <z@gosk.in>
2014-06-28 18:12:45 +0400

    log-after-response hook

--- src/erdico_log.erl
+++ src/erdico_log.erl
@@ -1,11 +1,12 @@
 -module(erdico_log).
 -export([access_log_hook/4]).
 
-access_log_hook(Status, _Headers, _Body, Req) ->
-    {{PeerAddr, _}, Req1} = cowboy_req:peer(Req),
-    {Method, Req2} = cowboy_req:method(Req1),
-    {Url, Req3} = cowboy_req:url(Req2),
+access_log_hook(Status, Headers, Body, Req) ->
+    {[{PeerAddr, _}, Method, Url], Req2} = lists:mapfoldl(fun get_req_prop/2, Req, [peer, method, url]),
+    {ok, ReqReplied} = cowboy_req:reply(Status, Headers, Body, Req2),
     PeerStr = inet_parse:ntoa(PeerAddr),
     lager:info([{tag, access}, {peer, PeerStr}, {method, Method}, {url, Url}, {status, Status}], ""),
-    Req3.
+    ReqReplied.
 
+get_req_prop(Prop, Req) ->
+        cowboy_req:Prop(Req).
