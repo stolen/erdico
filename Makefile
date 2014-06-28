cbd72ad8a636bf71ad2ccf385ebe1809f6cf5aff
Danil Zagoskin <z@gosk.in>
2014-06-28 23:52:15 +0400

    ubuntu package with conf-to-environment wrapper

--- Makefile
+++ Makefile
@@ -6,3 +6,6 @@ dep_cowboy = pkg://cowboy 0.10.0
 dep_lager = https://github.com/basho/lager.git 2.0.3
 
 include erlang.mk
+
+deb:
+	$(MAKE) -C pkg/$(PROJECT) deb
---
+++ pkg/erdico/conf_erdico.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+APP=erdico
+LOG=/var/log/$APP
+HOME=/var/lib/$APP
+
+export NL="
+"
+export RELX_REPLACE_OS_VARS=1   # Expand vars in config
+export RUNNER_LOG_DIR=$LOG
+export VMARGS_PATH=$HOME/vm.args
+export CONFIG_PATH=$HOME/sys.config
+
+# Read config
+export CLUSTERNODES=$(sh -c '. /etc/'$APP'.conf; for h in $CLUSTERHOSTS; do echo "'\'$APP'@$h'\''"; done | paste -sd, -')
+# save hostname
+export FQDN=$(hostname -f)
+
+exec `dirname $0`/$APP $@
