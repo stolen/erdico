33b6a08aae08a3c2d097d094c95b5d467832eea1
Danil Zagoskin <z@gosk.in>
2014-06-28 16:39:25 +0400

    increment counter in request handler and return its value in body

--- src/erdico_handler.erl
+++ src/erdico_handler.erl
@@ -7,7 +7,9 @@ init(_Type, Req, _Options) ->
     {ok, Req, nostate}.
 
 handle(Req, nostate) ->
-    {ok, Replied} = cowboy_req:reply(200, [], <<"hello\n">>, Req),
+    {ok, CounterValue} = erdico_counter:inc(),
+    ResponseBody = <<"value = ", (integer_to_binary(CounterValue))/binary, "\n">>,
+    {ok, Replied} = cowboy_req:reply(200, [], ResponseBody, Req),
     {ok, Replied, nostate}.
 
 terminate(_Reason, _Req, nostate) ->
